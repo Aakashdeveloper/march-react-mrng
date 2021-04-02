5f76e27dccfa69c5033f54143fab71e73ea5c6c7
aakashdeveloper <ahanda205@gmail.com>
2021-04-02 21:48:44 +0530

    a

--- mymobi/App.js
+++ mymobi/App.js
@@ -1,15 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState} from 'react';
+import { StyleSheet, Text, View, Button, Switch,Modal } from 'react-native';
 
-export default function App() {
+ function App() {
+   const [outputText,setText] = useState("Test Native App");
+   const [isEnabled, setIsEnabled] = useState(false);
+   const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+
   return (
     <View style={styles.container}>
+
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{outputText}</Text>
+      <Button title="Click Me" onPress={()=> setText('Text Changed')}/>
+      <Button title="Revert Me" onPress={()=> setText('Text Native App')}/>
+      <Switch
+        trackColor={{ false: "#767577", true: "#81b0ff" }}
+        thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
+        ios_backgroundColor="#3e3e3e"
+        onValueChange={toggleSwitch}
+        value={isEnabled}
+      />
     </View>
   );
 }
+
+export default App
 
 const styles = StyleSheet.create({
   container: {
